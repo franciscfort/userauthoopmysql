19dffae7ca09e4aff2fb2063273f24656b61449a
franciscfort <114653597+franciscfort@users.noreply.github.com>
2022-10-27 17:15:50 +0100

    Update route.php

--- classes/route.php
+++ classes/route.php
@@ -1,1 +1,25 @@
 <?php
+class signupcontr {
+  private  $uid;
+     private  $pwd;
+     private  $pwdRepeat; 
+     private  $email;
+  
+  public function  __connect($uid, $pwd, $pwdRepeat,$email) {
+    $this->$uid =$uid;
+    $this->$pwd =$pwd;
+    $this->$pwdRepeat =$pwdRepeat;
+    $this->$email =$email;
+  }
+  
+  private function  emptyinput() {
+    $result;
+    if (empty($this->$uid) || empty($this->$pwd) ||  empty($this->$pwdRepeat) ||  empty($this->$email)){
+      $result = false; 
+    } 
+      else {
+         $result = true;
+      
+      }
+    return $result;
+        }
